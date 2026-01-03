49d5e5550e88935349fab151ce16a592ea3a5e63
Luis1571 <55500525+Luis1571@users.noreply.github.com>
2026-01-03 17:36:27 +0100

    quitar Biome e instalar angular material

--- coins/src/app/app.component.ts
+++ coins/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatButtonModule],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
